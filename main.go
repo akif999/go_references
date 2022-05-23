a1af5a7597307b9a52dff18db84311bf828cf0e6
akif999 <aki01nf28@gmail.com>
2022-05-23 22:07:44 +0900

    Implement dumping top level of nodes

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+)
 
 func main() {
 	fmt.Println("Go")
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "./testdata/a.go", nil, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// ast.Print(fset, f)
+	for _, decl := range f.Decls {
+		fmt.Printf("%#v\n", decl)
+		switch decl.(type) {
+		case *ast.GenDecl:
+			fmt.Printf("  %#v\n", decl)
+		case *ast.FuncDecl:
+			fmt.Printf("  %#v\n", decl)
+		default:
+		}
+	}
 }
--- testdata/a.go
+++ testdata/a.go
@@ -2,9 +2,19 @@ package main
 
 import "fmt"
 
-var a int = 1
+var gVar1 int = 1
+
+func func1() {
+	var func1LVar1 string = "1"
+	func1Lvar2 := "2"
+
+	_ = func1LVar1
+	_ = func1Lvar2
+}
 
 func main() {
-	b := 2
-	fmt.Println(b)
+	var mainLVar1 int = 1
+	mainLVar2 := 2
+	fmt.Println(mainLVar1)
+	fmt.Println(mainLVar2)
 }

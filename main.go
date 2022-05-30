3d658127701172739691eb9f658b1d38edc3e471
akif999 <aki01nf28@gmail.com>
2022-05-30 23:59:23 +0900

    add cases of statements

--- main.go
+++ main.go
@@ -24,10 +24,36 @@ func main() {
 		case *ast.GenDecl:
 			// GenDecl has no referencing variable
 		case *ast.FuncDecl:
+			// Function Declaration Level
 			// fmt.Printf("  %#v\n", decl)
 			fmt.Printf("func: %s\n", decl.(*ast.FuncDecl).Name)
 			for _, stmt := range decl.(*ast.FuncDecl).Body.List {
 				fmt.Printf("  stmt: %#v\n", stmt)
+				switch stmt.(type) {
+				case *ast.BlockStmt:
+					// BlockStmt needs to recurse (BlockStmt)
+				case *ast.ExprStmt:
+				case *ast.DeclStmt:
+					// DeclStmt has no referencing variable
+				case *ast.AssignStmt:
+				case *ast.ReturnStmt:
+				case *ast.IncDecStmt:
+				case *ast.ForStmt:
+				case *ast.RangeStmt:
+				case *ast.IfStmt:
+				case *ast.SwitchStmt:
+				case *ast.TypeSwitchStmt:
+				case *ast.LabeledStmt:
+					// BlockStmt needs to recurse (Stmt)
+				case *ast.BranchStmt:
+					// DeclStmt has no referencing variable
+				case *ast.DeferStmt:
+				case *ast.GoStmt:
+				case *ast.SelectStmt:
+				case *ast.SendStmt:
+				case *ast.EmptyStmt:
+					// DeclStmt has no referencing variable
+				}
 			}
 		default:
 		}

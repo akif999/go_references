d384d8901529c89c4e0ded6d70446c3dfd180c44
akif999 <aki01nf28@gmail.com>
2022-12-22 14:13:25 +0900

    update testdata

--- testdata/a.go
+++ testdata/a.go
@@ -4,12 +4,14 @@ import "fmt"
 
 var gVar1 int = 1
 
-func func1() {
+func func1() string {
 	var func1LVar1 string = "1"
 	func1Lvar2 := "2"
 
 	_ = func1LVar1
 	_ = func1Lvar2
+
+	return func1LVar1
 }
 
 func main() {
@@ -17,4 +19,6 @@ func main() {
 	mainLVar2 := mainLVar1
 	fmt.Println(mainLVar1)
 	fmt.Println(mainLVar2)
+
+	return mainVar2
 }

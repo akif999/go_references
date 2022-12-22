8626a1a4a1cf14faaa2a4a344cd68cafa9a4049e
akif999 <aki01nf28@gmail.com>
2022-12-22 15:15:02 +0900

    add cases

--- stmt.go
+++ stmt.go
@@ -7,28 +7,33 @@ import (
 
 func parseStatement(stmt ast.Stmt) (variableReferences, error) {
 	switch stmt.(type) {
+	case *ast.BadStmt:
+		// Notice: Since an error occurs in the package processing that acquires Go's AST, there is no error here actually.
+		return variableReferences{}, fmt.Errorf("input files has bad declaration at line %d", getLine(fset, stmt.(*ast.BadStmt).From))
+	case *ast.DeclStmt:
+		// DeclStmt has no referencing variable
+	case *ast.EmptyStmt:
+	case *ast.LabeledStmt:
+	case *ast.ExprStmt:
+	case *ast.SendStmt:
+	case *ast.IncDecStmt:
+	case *ast.AssignStmt:
+		parseAssignStmt(stmt.(*ast.AssignStmt))
+	case *ast.GoStmt:
+	case *ast.DeferStmt:
+	case *ast.ReturnStmt:
+	case *ast.BranchStmt:
 	case *ast.BlockStmt:
 		// BlockStmt needs to recurse (BlockStmt)
 		parseBlockStmt(stmt.(*ast.BlockStmt))
-	case *ast.ExprStmt:
-	case *ast.DeclStmt:
-		// DeclStmt has no referencing variable
-	case *ast.AssignStmt:
-		parseAssignStmt(stmt.(*ast.AssignStmt))
-	case *ast.ReturnStmt:
-	case *ast.IncDecStmt:
-	case *ast.ForStmt:
-	case *ast.RangeStmt:
 	case *ast.IfStmt:
+	case *ast.CaseClause:
 	case *ast.SwitchStmt:
 	case *ast.TypeSwitchStmt:
-	case *ast.LabeledStmt:
-	case *ast.BranchStmt:
-	case *ast.DeferStmt:
-	case *ast.GoStmt:
+	case *ast.CommClause:
 	case *ast.SelectStmt:
-	case *ast.SendStmt:
-	case *ast.EmptyStmt:
+	case *ast.ForStmt:
+	case *ast.RangeStmt:
 		// EmptyStmt has no referencing variable
 	default:
 		return variableReferences{}, fmt.Errorf("invalid ast element: %+v", stmt)
